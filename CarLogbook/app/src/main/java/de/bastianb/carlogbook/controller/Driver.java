91b209dc4325e57315a9ebdd367e100a16b2723f
Bastian Bräunel <bastian.braeunel@gmx.de>
2016-02-03 15:15:20 +0100

    Erfolgreich Ormlite als Abhängigkeit eingebunden

--- CarLogbook/app/src/main/java/de/bastianb/carlogbook/controller/Driver.java
+++ CarLogbook/app/src/main/java/de/bastianb/carlogbook/controller/Driver.java
@@ -1,7 +1,4 @@
 package de.bastianb.carlogbook.controller;
 
-/**
- * Created by santa on 03.02.16.
- */
 public class Driver {
 }
--- CarLogbook/app/src/main/java/de/bastianb/carlogbook/model/Ride.java
+++ CarLogbook/app/src/main/java/de/bastianb/carlogbook/model/Ride.java
@@ -1,7 +1,11 @@
 package de.bastianb.carlogbook.model;
 
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
- * Created by santa on 03.02.16.
+ * This class is the represents the model for all the rides in the database
+ * mapped with ormlite to sqlite
  */
+@DatabaseTable(tableName = "Rides")
 public class Ride {
 }
