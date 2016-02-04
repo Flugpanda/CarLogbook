3db3bbf469aa88d020f1cc0a250f66f4044a16f8
Bastian Bräunel <bastian.braeunel@gmx.de>
2016-02-04 11:45:07 +0100

    Konfiguration für ormlite generiert

--- CarLogbook/app/src/main/java/de/bastianb/carlogbook/control/DatabaseConfigUtil.java
+++ CarLogbook/app/src/main/java/de/bastianb/carlogbook/control/DatabaseConfigUtil.java
@@ -1,7 +1,19 @@
 package de.bastianb.carlogbook.control;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
+
 /**
- * Created by santa on 03.02.16.
+ * DatabaseConfigUtl writes a configuration file to avoid using annotation processing in runtime which is very slow
+ * under Android. This gains a noticeable performance improvement.
+ *
+ * The configuration file is written to /res/raw/ by default. More info at: http://ormlite.com/docs/table-config
  */
-public class DatabaseConfigUtil {
+public class DatabaseConfigUtil extends OrmLiteConfigUtil {
+
+    public static void main(String[] args) throws SQLException, IOException {
+        writeConfigFile("ormlite_config.txt");
+    }
 }
